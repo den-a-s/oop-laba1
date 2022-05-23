7c2dcde112b950a2e2bfaf4fb0854b31027ab9a3
Денис Смирнов <pr13RACK@yandex.ru>
2022-05-23 13:29:42 +0300

    Добавление класса в котором хранятся данные о пациентах

---
+++ patientdata.cpp
@@ -0,0 +1,64 @@
+#include "patientdata.h"
+
+PatientData::PatientData()
+{}
+
+PatientData::~PatientData()
+{
+    for (const auto &item: m_patients) {
+        delete item;
+    }
+}
+
+PatientData &PatientData::instance()
+{
+    static PatientData inst;
+    return inst;
+}
+
+void PatientData::print() const
+{
+    for (const auto &item: m_patients) {
+        item->print();
+        QTextStream(stdout) << '\n';
+    }
+}
+
+void PatientData::loadData()
+{
+    QString path = QStandardPaths::locate(QStandardPaths::HomeLocation, "patiens_data.json");
+    QFile loadFile(path);
+
+    if (!loadFile.exists()) throw "Bruh";
+    if (!loadFile.open(QIODevice::ReadOnly)) {
+        qWarning("Couldn't open save file.");
+        throw "Bruh";
+    }
+    QByteArray saveData = loadFile.readAll();
+    QJsonDocument loadDoc(QJsonDocument::fromJson(saveData));
+    readPatients(loadDoc.object());
+}
+
+void PatientData::readPatients(const QJsonObject &json)
+{
+    if (json.contains("patients") && json["patients"].isArray()) {
+        QJsonArray patients = json["patients"].toArray();
+        m_patients.clear();
+        m_patients.reserve(patients.size());
+        for (const auto &item: patients) {
+            AbstractAnimal* animal = getAnimal(item.toObject());
+            m_patients.push_back(animal);
+        }
+    }
+}
+
+AbstractAnimal *PatientData::getAnimal(const QJsonObject &json)
+{
+    AbstractAnimal *animal = nullptr;
+    if (json.contains("who") && json["who"] == "cat")
+        animal = new Cat();
+    else if (json.contains("who") && json["who"] == "dog")
+        animal = new Dog();
+    if (animal != nullptr) animal->read(json);
+    return animal;
+}
---
+++ patientdata.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <QStandardPaths>
+#include <QString>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+
+#include "cat.h"
+#include "dog.h"
+
+class PatientData
+{
+public:
+    PatientData(const PatientData&) = delete;
+    void operator=(const PatientData&) = delete;
+    static PatientData& instance();
+
+    void print() const;
+private:
+    void loadData();
+    void readPatients(const QJsonObject &json);
+    AbstractAnimal *getAnimal(const QJsonObject &json);
+    PatientData();
+    ~PatientData();
+    std::vector<AbstractAnimal*> m_patients;
+};
