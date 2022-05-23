ce308a8d9f45516a0ea7e4a796ba5e01826c8d90
Денис Смирнов <pr13RACK@yandex.ru>
2022-05-23 14:03:18 +0300

    Исправление ошибки с шаблонами

--- myalgorithm.h
+++ myalgorithm.h
@@ -1,4 +1,10 @@
 #pragma once
-#include <QString>
 
-int findIdx(const QString array[]);
+template<class T>
+int findIdx(const T &item, const T *array, int sizeArray)
+{
+    for (int i = 0; i < sizeArray; ++i) {
+        if (array[i] == item) return i;
+    }
+    return -1;
+}
