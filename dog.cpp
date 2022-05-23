59b8614640f752d83cc165bc9333ea6c61d656ff
Денис Смирнов <pr13RACK@yandex.ru>
2022-05-23 13:27:13 +0300

    Добавление класса Собаки

---
+++ dog.cpp
@@ -0,0 +1,67 @@
+#include "dog.h"
+
+Dog::Dog()
+{}
+
+Dog::Dog(const QString &name,  int breed, int disease)
+    :m_name(name), m_breed(breed), m_disease(disease)
+{}
+
+Dog::Dog(const Dog &cat)
+{
+    m_name = cat.m_name;
+    m_breed = cat.m_breed;
+    m_disease = cat.m_disease;
+}
+
+Dog &Dog::operator=(const Dog &cat)
+{
+    if (this != &cat) {
+        m_name = cat.m_name;
+        m_breed = cat.m_breed;
+        m_disease = cat.m_disease;
+    }
+    return *this;
+}
+
+Dog::~Dog()
+{}
+
+void Dog::read(const QJsonObject &json)
+{
+    if (json.contains("name") && json["name"].isString()) {
+        m_name = json["name"].toString();
+    }
+    if (json.contains("breed") && json["breed"].isString()) {
+        QString tmpBreed = json["name"].toString();
+        m_breed = findIdx(tmpBreed, dogsBreeds, DOGS_BREEDS_SIZE);
+    }
+    if (json.contains("disease") && json["disease"].isString()) {
+        QString tmpDisease = json["disease"].toString();
+        m_breed = findIdx(tmpDisease, dogsDisease, DOGS_DISEASE_SIZE);
+    }
+}
+
+void Dog::setName(const QString &newName)
+{
+    m_name = newName;
+}
+
+void Dog::setBreed(int newBreed)
+{
+    m_breed = newBreed;
+}
+
+void Dog::setDisease(int newDisease)
+{
+    m_disease = newDisease;
+}
+
+void Dog::print() const
+{
+    QTextStream out(stdout);
+
+    out << m_name << ":" << m_breed << ":" << m_disease;
+}
+
+
---
+++ dog.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "abstractanimal.h"
+#include "myalgorithm.h"
+
+#include <algorithm>
+
+#include <QTextStream>
+
+#define DOGS_DISEASE_SIZE 4
+#define DOGS_BREEDS_SIZE 9
+
+const static QString dogsDisease[DOGS_DISEASE_SIZE] {
+    QString::fromLocal8Bit("Бешенство"),
+    QString::fromLocal8Bit("Лептоспироз"),
+    QString::fromLocal8Bit("Вольерный кашель"),
+    QString::fromLocal8Bit("Болезнь Карре")
+};
+const static QString dogsBreeds[DOGS_BREEDS_SIZE] {
+    QString::fromLocal8Bit("Алабай"),
+    QString::fromLocal8Bit("Хаски"),
+    QString::fromLocal8Bit("Бульдог"),
+    QString::fromLocal8Bit("Боксер"),
+    QString::fromLocal8Bit("Корги"),
+    QString::fromLocal8Bit("Мопс"),
+    QString::fromLocal8Bit("Питбуль"),
+    QString::fromLocal8Bit("Спаниель"),
+    QString::fromLocal8Bit("Чихуахуа")
+};
+
+class Dog: public AbstractAnimal {
+public:
+    Dog();
+    Dog(const QString &name, int breed, int disease);
+    Dog(const Dog &cat);
+    Dog &operator=(const Dog &cat);
+    ~Dog();
+
+    void read(const QJsonObject &json) override;
+
+    void setName(const QString &newName) override;
+
+    void setBreed(int newBreed) override;
+
+    void setDisease(int newDisease) override;
+
+    void print() const override;
+
+private:
+    QString m_name;
+    int m_breed;
+    int m_disease;
+};
